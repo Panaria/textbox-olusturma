2cc7232a66cda2c41cede08b582bedcb4851706d
Panaria <baranyldzby@gmail.com>
2023-10-23 20:34:14 +0300

    Add project files.

---
+++ textbox olusturma/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace textbox_olusturma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TextBox txt = new TextBox();
+            txt.Text = "Ronaldo";
+            txt.Top = 200;
+            txt.Left = 275;
+            txt.Width = 300;
+
+            this.Controls.Add(txt);
+        }
+    }
+}
